b3b596af501534886706638929b482ab82ad5a3b
arjunamarcelino <13519021@std.stei.itb.ac.id>
2021-07-10 08:59:50 +0700

    init

---
+++ main.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+PATH = ".\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+# open the web
+driver.implicitly_wait(5)
+
+driver.maximize_window()
+
+driver.get("https://www.tiket.com/")
+
+# choose product
+driver.find_element(By.ID, "productSearchFrom").send_keys("KNO")  # origin
+wait = WebDriverWait(driver, 10)
+element = wait.until(EC.element_to_be_clickable(
+    (By.XPATH, "//*[@id='fromDropDownList-airport1']")))
+element.click()
+# driver.find_element_by_xpath("//*[@id='fromDropDownList-airport1']").click()
+time.sleep(2)
+
+# driver.find_element(By.ID, "productSearchTo").send_keys("CGK")  # dest
+
+# driver.find_element_by_id("productSearchDeparture").clear()
+# driver.find_element_by_id(
+#     "productSearchDeparture").send_keys("Sen, 26 Jul 2021")
+
+# driver.find_element_by_id("productSearchReturn").clear()
+# driver.find_element_by_id("productSearchReturn").send_keys("Rab, 28 Jul 2021")
+
+
+# submit product
+# driver.find_element_by_xpath("//*[@id='productWidget']/div[2]/div[3]/button").click()
+
+
+time.sleep(10)
+
+# closes all the browsers
+driver.quit()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+src2 = driver.find_element_by_id("fromDropDownList-airport1")
+src2.click()
